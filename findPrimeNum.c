290da42c83f5028e8ea98e09ec66ce2199bf882c
ytliang97 <ytliang97@gmail.com>
2017-12-21 17:34:28 +0800

    update findPrimeNum

--- findPrimeNum.c
+++ findPrimeNum.c
@@ -14,34 +14,43 @@ int test(int);
 int main(int argc, char* argv[]) {
     clock_t startTime, endTime;
     float totalTime = 0.0;
-    
+
     int input;
     scanf("%d", &input);
-    
+
     startTime = clock();
-    
-    for( int i = 2 ; i <= input ; i++ ) {
-        int isPrim = test(i);
-        int count = 0;
-        if(isPrim) {
-            count++;
-            printf("%d\n", i);
-        }        
+    int count = 0;
+    if(input <= 2) {
+        printf("2 ");
+        count++;
+    }
+    else if(input > 2) {
+        printf("2 ");
+        count++;
+        for( int i = 3 ; i <= input ; i++ ) {
+            int isPrim = test(i);
+            if(isPrim) {
+                count++;
+                printf("%d ", i);
+            }
+        }
     }
     endTime = clock();
     totalTime = (float) (endTime-startTime) / CLOCKS_PER_SEC;
-    printf("take %.2f sec\n", totalTime);
+    printf("\nfrom 0 ~ %d, there are %d prime number, take %.2f sec\n", input, count, totalTime);
     return 0;
 }
 
 int test(int num) {
-    int sqrt_num = (int) sqrt(num)
+//    int sqrt_num = (int) sqrt(num);
     int isPrim = 1;
     for(int i = 2 ; i < num ; i++ ) {
         if(num%i == 0) {
             isPrim = 0;
             break;
         }
+        else if (num == (i + 1))
+            break;
     }
 
     return isPrim;
