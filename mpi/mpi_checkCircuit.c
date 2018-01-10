75efd3fafa9dd93f132ea72782a876ff81b06a15
ytliang97 <ytliang97@gmail.com>
2018-01-10 20:09:01 +0800

    update mpi_primeSieve.c. Complete!

--- mpi/mpi_checkCircuit.c
+++ mpi/mpi_checkCircuit.c
@@ -5,12 +5,12 @@
 int checkCircuit(int []);
 int print(int, int []);
 int main(int argc, char* argv[]){
-	
-	int circuit[16];//circuit
-	for(int i = 0;i < 16;i++) {
-		circuit[i]=0;	
-	}
-    /*	a)save 16 bit 0/1 in the circuit array. save integer from 0 to 65535
+
+    int circuit[16];//circuit
+    for(int i = 0;i < 16;i++) {
+        circuit[i]=0;
+    }
+    /*  a)save 16 bit 0/1 in the circuit array. save integer from 0 to 65535
             a.1) divide 65535 to process 0 ~ p.
                 E.g. divide 4(2) process:
                     process[0(0)] compute 0(0),4(2),8(4),...
@@ -18,11 +18,11 @@ int main(int argc, char* argv[]){
                     process[2] compute 2,6,10,...
                     process[3] compute 3,7,11,...
 
-    	b)store bit from highest to lowest in binary to circuit array
+        b)store bit from highest to lowest in binary to circuit array
         c)final: compute circuit to get output, and print the output
             c.1) let process id) circuit
             c.2) let process[0] print total available and total time.
-	*/
+    */
     int count = 0;
     int global_count = 0;
     // a)
@@ -39,51 +39,52 @@ int main(int argc, char* argv[]){
         printf("%d", pnum);
     }
 */
-    for(int i = pid;i < 65536;i+=pnum) {
-        
-        // b)
-        int j=0;
-	  	for(int k = 15; k >= 0; k--) {
-	   		int binary = (i >> k) & 1;
-	   		circuit[j] = binary;
-	   		j++;		
-	  	}
-		
-		// c)
-		int output = checkCircuit(circuit);
-		if (output == 1) {
-            // c.1)
-            print(pid, circuit);
-            count++;
+for(int i = pid;i < 65536;i+=pnum) {
+    
+            // b)
+            int j=0;
+            for(int k = 15; k >= 0; k--) {
+                int binary = (i >> k) & 1;
+                circuit[j] = binary;
+                j++;
+            }
+    
+            // c)
+            int output = checkCircuit(circuit);
+            if (output == 1) {
+                // c.1)
+                print(pid, circuit);
+                count++;
+            }
         }
+    
+        // c.2)
+        MPI_Reduce(&count, &global_count, 1, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);/*collect total available*/
+        t2 = MPI_Wtime();
+        double ptime = t2 - t1;
+        double global_time;
+        MPI_Reduce(&ptime, &global_time, 1, MPI_DOUBLE, MPI_SUM, root, MPI_COMM_WORLD);/*total process spend on computing*/
+    //    printf("\n%d)%f\n", pid, ptime);
+        MPI_Barrier(MPI_COMM_WORLD);
+        printf("Process %d is done\n", pid);
+        if(pid == 0) {
+            printf("total: %d\n", global_count);
+            printf("average take %.3f time\n", global_time/pnum);
+        }
+        MPI_Finalize();
+    
+        return 0;
     }
     
-    // c.2)
-    MPI_Reduce(&count, &global_count, 1, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);/*collect total available*/
-    t2 = MPI_Wtime();
-    double ptime = t2 - t1;
-    double global_time;
-    MPI_Reduce(&ptime, &global_time, 1, MPI_DOUBLE, MPI_SUM, root, MPI_COMM_WORLD);/*total process spend on computing*/
-//    printf("\n%d)%f\n", pid, ptime);    
-    MPI_Barrier(MPI_COMM_WORLD);
-    if(pid == 0) {
-        printf("Process 0 is done\n");
-        printf("total: %d\n", global_count);
-        printf("average take %.3f time\n", global_time/pnum);
+    int checkCircuit(int circuit[]) {
+        int output = (circuit[0] | circuit[1]) & (!circuit[1] | !circuit[3]) & (circuit[2] | circuit[3]) & (!circuit[3] | !circuit[4]) & (circuit[4] | !circuit[5]) & (circuit[5] | circuit[6]) & (circuit[5] | !circuit[6]) & (circuit[7] | !circuit[8]) & (circuit[8] | circuit[9]) & (circuit[8] | !circuit[9]) & (!circuit[9] | !circuit[10]) & (circuit[10] | circuit[11]) & (circuit[11] | circuit[9]) & (circuit[12] | circuit[13]) & (!circuit[7] | !circuit[13]) & (circuit[13] | !circuit[14]) & (circuit[14] | circuit[15]) & (!circuit[15] | circuit[6]);
+        return output;
     }
-    MPI_Finalize();
-
-	return 0;
-}
-
-int checkCircuit(int circuit[]) {
-    int output = (circuit[0] | circuit[1]) & (!circuit[1] | !circuit[3]) & (circuit[2] | circuit[3]) & (!circuit[3] | !circuit[4]) & (circuit[4] | !circuit[5]) & (circuit[5] | circuit[6]) & (circuit[5] | !circuit[6]) & (circuit[7] | !circuit[8]) & (circuit[8] | circuit[9]) & (circuit[8] | !circuit[9]) & (!circuit[9] | !circuit[10]) & (circuit[10] | circuit[11]) & (circuit[11] | circuit[9]) & (circuit[12] | circuit[13]) & (!circuit[7] | !circuit[13]) & (circuit[13] | !circuit[14]) & (circuit[14] | circuit[15]) & (!circuit[15] | circuit[6]);
-    return output;
-}
-int print(int pid, int circuit[]) {
-    printf("%d)", pid);
-    for(int k=0;k<16;k++){
-        printf("%d", circuit[k]);
+    int print(int pid, int circuit[]) {
+        printf("%d)", pid);
+        for(int k=0;k<16;k++){
+            printf("%d", circuit[k]);
+        }
+        printf("\n");
     }
-    printf("\n");
-}
+    
--- mpi/mpi_primeSieve.c
+++ mpi/mpi_primeSieve.c
@@ -11,48 +11,73 @@
 #include <time.h>
 #define unmarked 0
 #define marked 1
+#define root 0
+#define notprime 2
 int main(int argc, char* argv[]) {
     int N = strtol(argv[1], NULL, 10);
-    
+
     // 1)
     int *list = (int *) malloc((N+1)*sizeof(int));
     for(int i = 0;i <= N;i++)
         *(list + i) = unmarked;
-    
+    *(list) = notprime;
+    *(list + 1) = notprime;
     // 2)
     int k = 2;
-    
-    clock_t t2,t1;
-    t1 = clock();
+
+    int pid;
+    int pnum;
+    MPI_Init(&argc,&argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+    MPI_Comm_size(MPI_COMM_WORLD, &pnum);
+
+    double t2,t1;
+    t1 = MPI_Wtime();
     // 3)
     while(1) {
         if(k*k < N) {
-           for(int i = k+1;i <= N;i++) { // 3.a)
-                if(i % k == 0)   
+            for(int i = (N/pnum) * pid + 1;i <= (pid == (pnum - 1)?N:(N / pnum) * (pid + 1));i++) { // 3.a)
+                /*if(i == 1) continue; not need this anymore(use notprime)*/
+                if(i % k == 0 && i != k && *(list + i) == unmarked) {
                     *(list + i) = marked;
-           }
-           for(int i = k+1;i <= N;i++) { // 3.b)
-               if(*(list + i) == unmarked) {
-                   k = i;
-                   break;
-               }
-           }
+                }
+            }
+///            if(pid == root) { //--why?
+                for(int i = k+1;i <= N;i++) { // 3.b)
+                    if(*(list + i) == unmarked) {
+///                     printf("origin k: %d\n", k);
+                        k = i;
+                        MPI_Bcast(&k, 1, MPI_INT, root, MPI_COMM_WORLD);
+///                     printf("-------------%d\n", k);
+                        break;
+                    }
+                }
+///            }
+///     printf("k*k = %d\n",k*k);
         }
         else break; // 3.c)
     }
-
+    MPI_Barrier(MPI_COMM_WORLD);
     // 4)
     int count = 0;
-    for(int i = 2;i <= N;i++) {
+    int global_count = 0;
+    for(int i = (N/pnum) * pid + 1;i <= (pid == (pnum - 1)?N:(N / pnum) * (pid + 1));i++) {
         if(*(list + i) == unmarked) {
-//            printf("%d ", i);
+///            printf("%d ", i);
             count++;
         }
     }
-    t2 = clock();
-    float global_time = (float)(t2 - t1) / CLOCKS_PER_SEC;
-    printf("total prime: %d\n", count);
-    printf("total take %.3f sec\n", global_time);
+    t2 = MPI_Wtime();
+    double ptime = t2 - t1;
+    double global_time;
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Reduce(&count, &global_count, 1, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
+    MPI_Reduce(&ptime, &global_time, 1, MPI_DOUBLE, MPI_SUM, root, MPI_COMM_WORLD);
+    if(pid == root) {
+        printf("total prime: %d\n", global_count);
+        printf("total take %.3f sec\n", global_time/pnum);
+    }
+    MPI_Finalize();
     free(list);
     return 0;
 }
